d9ed8527bd7317757743e62c37b0501f0ae61b7f
Abimael <abimaelcarvalho95@gmail.com>
2024-05-22 17:05:36 -0300

    "Initial commit

---
+++ src/main/java/com/br/BrazaGoat/BrazaGoatApplication.java
@@ -0,0 +1,13 @@
+package com.br.BrazaGoat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrazaGoatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrazaGoatApplication.class, args);
+	}
+
+}
