e91b04389ad030191802f219e24aa3b2b643d8a9
Abimael <abimaelcarvalho95@gmail.com>
2024-05-27 18:36:09 -0300

    feat: Atualizações na JogadoreRepository
    - Adicionado o List findByStatus para fazer buscar no metodo jogadoresAtivos para buscar jogadores com status (true) ativos no banco de dados.

--- src/main/java/com/br/BrazaGoat/repositories/JogadorRepository.java
+++ src/main/java/com/br/BrazaGoat/repositories/JogadorRepository.java
@@ -1,12 +1,15 @@
 package com.br.BrazaGoat.repositories;
 
 import com.br.BrazaGoat.model.JogadorModel;
+import com.br.BrazaGoat.model.StatusJogadorPartida;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface JogadorRepository extends JpaRepository<JogadorModel, UUID> {
 
+    List<JogadorModel> findByStatus(boolean status);
 }
