3efa600f023b60fa7360dfa6110e4938f009ed55
Abimael <abimaelcarvalho95@gmail.com>
2024-06-26 15:49:07 -0300

    feat: Adicionar extensão do JpaRepository e método de busca por partida em GolRepository
    - Extender GolRepository para JpaRepository<GolModel, Long>.
    - Adicionar método findByPartida(PartidaModel partida) para buscar gols por partida específica.

--- src/main/java/com/br/BrazaGoat/partidas/gol/GolRecordDTO.java
+++ src/main/java/com/br/BrazaGoat/partidas/gol/GolRecordDTO.java
@@ -1,4 +1,22 @@
 package com.br.BrazaGoat.partidas.gol;
 
-public record GolRecordDTO() {
+import com.br.BrazaGoat.jogador.entities.JogadorModel;
+import com.br.BrazaGoat.partidas.assistencia.AssistenciaModel;
+import com.br.BrazaGoat.partidas.partida.PartidaModel;
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record GolRecordDTO(
+        Long id,
+        JogadorModel jogador,
+        AssistenciaModel assistencia,
+        PartidaModel partida,
+        int numeroDoGol,
+        LocalTime horaDoGol,
+        LocalDate dataDoGol,
+        int pontoPorGol,
+        int pontoPorGolContra
+) {
 }
--- src/main/java/com/br/BrazaGoat/partidas/gol/GolRepository.java
+++ src/main/java/com/br/BrazaGoat/partidas/gol/GolRepository.java
@@ -1,4 +1,12 @@
 package com.br.BrazaGoat.partidas.gol;
 
-public interface GolRepository {
+import com.br.BrazaGoat.partidas.partida.PartidaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface GolRepository extends JpaRepository<GolModel, Long> {
+    List<GolModel> findByPartida(PartidaModel partida);
 }
